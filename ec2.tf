180f48ae834c71d7cf9d3560d74b38b5d2b932e2
ashemesh <ashemesh@akamai.com>
2023-06-29 10:49:42 +0300

    my second commit

--- ec2.tf
+++ ec2.tf
@@ -25,15 +25,15 @@ resource "aws_instance" "ec2_lcoals_demo" {
 #  }
 }
 
-#resource "aws_instance" "ec2_demo_test_2" {
-#  for_each = var.availability_zone
-#
-#  ami               = "ami-0e820afa569e84cc1"
-#  instance_type     = var.ec2_instance_type
-#  availability_zone = each.value
-#
-#  tags = {
-#    Name = "For-each-Demo-${random_string.id.result}"
-#  }
-#}
+resource "aws_instance" "ec2_demo_test_2" {
+  for_each = var.availability_zone
+
+  ami               = "ami-0e820afa569e84cc1"
+  instance_type     = var.ec2_instance_type
+  availability_zone = each.value
+
+  tags = {
+    Name = "For-each-Demo-${random_string.id.result}"
+  }
+}
 
