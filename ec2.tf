1730e6bc3be9b5b7d8b3f949ff476bdf72584f64
ashemesh <ashemesh@akamai.com>
2023-06-29 13:13:59 +0300

    another commit 2

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,21 @@
 resource "aws_instance" "ec2_demo" {
   count             = 1
-  ami               = "ami-0e820afa569e84cc1"
+  ami               = "ami-03f38e546e3dc59e1"
   instance_type     = var.ec2_instance_type
   availability_zone = var.availability_zone.a
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.main.id
+  security_groups = [aws_security_group.main.id]
+  user_data = <<EOF
+      #!/bin/bash
+      # Use this for your user data (script from top to bottom)
+      # install httpd (Linux 2 version)
+      yum update -y
+      yum install -y httpd
+      systemctl start httpd
+      systemctl enable httpd
+      echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+EOF
 
   tags = {
     Name = "Demo-${random_string.id.result}"
@@ -12,18 +25,18 @@ resource "aws_instance" "ec2_demo" {
 #  }
 }
 
-resource "aws_instance" "ec2_lcoals_demo" {
-  ami               = local.locals-ami
-  instance_type     = var.ec2_instance_type
-  availability_zone = var.availability_zone.b
-
-  tags = {
-    Name = "Locals-Demo-${random_string.id.result}"
-  }
+#resource "aws_instance" "ec2_lcoals_demo" {
+#  ami               = local.locals-ami
+#  instance_type     = var.ec2_instance_type
+#  availability_zone = var.availability_zone.b
+#
+#  tags = {
+#    Name = "Locals-Demo-${random_string.id.result}"
+#  }
 #  lifecycle {
 #    prevent_destroy = true
 #  }
-}
+#}
 
 #resource "aws_instance" "ec2_demo_test_2" {
 #  for_each = var.availability_zone
